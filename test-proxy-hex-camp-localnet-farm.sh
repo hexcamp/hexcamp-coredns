6b26c9cce31acb3f9e85d253898f80a365d52cdd
Jim Pick <jim@jimpick.com>
2024-05-26 20:36:41 -0700

    Update test and .gitignore

--- test-proxy-hex-camp-localnet-farm.sh
+++ test-proxy-hex-camp-localnet-farm.sh
@@ -3,20 +3,21 @@
 #q @jpimac.local:1053 A uxirkffr.hex.camp
 
 
-echo Query uxirkffr.test.hex.camp from first DOH via proxy
+#echo Query uxirkffr.test.hex.camp from first DOH via proxy
 
-q @dns1000.localhost:53 A uxirkffr.test.hex.camp
+#q @dns1000.localhost:53 A uxirkffr.test.hex.camp
 
-echo
+#echo
 
-echo Query 3.4.5.4.2.4.2.1.2.4.46.h3.test.hex.camp from second DOH via proxy
+#echo Query 3.4.5.4.2.4.2.1.2.4.46.h3.test.hex.camp from second DOH via proxy
 
-q @dns-doh2-localnet-farm-5:53 A 3.4.5.4.2.4.2.1.2.4.46.h3.test.hex.camp
+#q @dns-doh2-localnet-farm-5:53 A 3.4.5.4.2.4.2.1.2.4.46.h3.test.hex.camp
 
-echo
+#echo
 
-echo Query uxirkffr.test.hex.camp using live DNS via local unbound
+#echo Query uxirkffr.test.hex.camp using live DNS via local unbound
 
-q @127.0.0.1:2053 A uxirkffr.test.hex.camp
+#q @127.0.0.1:2053 A uxirkffr.test.hex.camp
+q @8.8.8.8 A uxirkffr.test.hex.camp
 
 

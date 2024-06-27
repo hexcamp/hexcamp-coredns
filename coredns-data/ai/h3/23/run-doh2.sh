43850a059c989e13ac337cfaf80f90a6deecdc09
Jim Pick <jim@jimpick.com>
2024-06-26 18:10:29 -0700

    Update zone files

--- coredns-data/ai/h3/23/run-doh2.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-#../coredns/coredns -conf Corefile-tls-forward
-#../coredns/coredns -conf Corefile-zone-file-example-org
-/coredns -conf Corefile-zone-file-23-h3-test-hex-camp
--- coredns-data/axpq/h3/46/run-doh2.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-#../coredns/coredns -conf Corefile-tls-forward
-#../coredns/coredns -conf Corefile-zone-file-example-org
-/coredns -conf Corefile-zone-file-46-h3-test-hex-camp

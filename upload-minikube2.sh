7fe61387b5441701ac56a08bef88426ff98152ef
Jim Pick <jim@jimpick.com>
2024-11-09 14:21:48 -0800

    Upload Corefile/zone to minikube2

---
+++ upload-minikube2.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+. SETENV
+
+if [ -z "$AWS_ACCESS_KEY_ID" ]; then
+  echo Need AWS_ACCESS_KEY_ID in SETENV
+fi
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+  echo Need AWS_SECRET_ACCESS_KEY in SETENV
+fi
+
+cd top-level-corefiles/minikube2
+
+rsync -vaP \
+  --exclude '*.template' \
+  --exclude .gitignore \
+  . minikube2:/home/centos/storage/coredns-test
+
+cd ../../coredns-data
+
+rsync -vaP \
+  axpq \
+  minikube2:/home/centos/storage/coredns-data
+
+

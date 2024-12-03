69d29c59eefd49f74b1d0b794a8d81b6110e2112
Jim Pick <jim@jimpick.com>
2024-12-02 21:59:34 -0800

    Update IPs

---
+++ watch-minikube7.sh
@@ -0,0 +1,1 @@
+while true; do kubectl --context minikube7 logs -f coredns-test; sleep 1; done
---
+++ watch-region-okinawa-as7q.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+CONTEXT=minikube7
+REGION=$1
+
+
+if [ -z "$REGION" ]; then
+  REGION=as7q
+fi
+
+while true; do echo -n .; kubectl --context $CONTEXT -n $REGION logs -f `kubectl --context $CONTEXT -n $REGION get pods -o json | jq -r '.items[0].metadata.name'` 2> /dev/null; sleep 1; done

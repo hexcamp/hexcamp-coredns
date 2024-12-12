128fd48d00cc8294539b28447bfd8b552768773f
Jim Pick <jim@jimpick.com>
2024-12-12 13:34:46 -0800

    Change IPs in zone files to templates

--- generator/generate.sh
+++ generator/generate.sh
@@ -1,27 +1,37 @@
 #! /bin/bash
 
-set -euxo pipefail
+#set -euxo pipefail
+set -euo pipefail
 
 gen_new_file() {
-  mkdir -p current/corefiles/minikube6
-  SERIAL=$(cat previous/corefiles/minikube6/db.test.hex.camp | sed -n 's,; serial,,p')
-  cat ips.json | jq ".serial=$SERIAL" | minijinja-cli -f json templates/corefiles/minikube6/db.test.hex.camp - > current/corefiles/minikube6/db.test.hex.camp
-  PREVSUM=$(cat previous/corefiles/minikube6/db.test.hex.camp | sha512sum)
-  CURSUM=$(cat current/corefiles/minikube6/db.test.hex.camp | sha512sum)
+  DIRNAME=$(dirname $1)
+  BASENAME=$(basename $1)
+  mkdir -p current/$DIRNAME
+  SERIAL=$(cat previous/$DIRNAME/$BASENAME | sed -n 's,; serial,,p')
+  if [ -z "$SERIAL" ]; then
+    cp templates/$DIRNAME/$BASENAME current/$DIRNAME/$BASENAME
+    return
+  fi
+  cat ips.json | jq ".serial=$SERIAL" | minijinja-cli -f json templates/$DIRNAME/$BASENAME - > current/$DIRNAME/$BASENAME
+  PREVSUM=$(cat previous/$DIRNAME/$BASENAME | sha512sum)
+  CURSUM=$(cat current/$DIRNAME/$BASENAME | sha512sum)
   if [ "$PREVSUM" != "$CURSUM" ]; then
     DATE=$(date +"%Y%m%d")
     if (echo $SERIAL | grep "^$DATE"); then
-      echo Same Date, increment
+      #echo Same Date, increment
       SERIAL=$((SERIAL + 1))
     else
-      echo New Date
+      #echo New Date
       SERIAL="${DATE}01"
     fi
     echo SERIAL: $SERIAL
-    cat ips.json | jq ".serial=$SERIAL" | minijinja-cli -f json templates/corefiles/minikube6/db.test.hex.camp - > current/corefiles/minikube6/db.test.hex.camp
-  PREVSUM=$(cat previous/corefiles/minikube6/db.test.hex.camp | sha512sum)
+    cat ips.json | jq ".serial=$SERIAL" | minijinja-cli -f json templates/$DIRNAME/$BASENAME - > current/$DIRNAME/$BASENAME
   fi
 }
 
-gen_new_file corefiles/minikube6/db.test.hex.camp
+for f in $(find templates -type f | sed 's,templates/,,'); do
+  #echo $f
+  gen_new_file $f
+  diff -u previous/$f current/$f || true
+done
 

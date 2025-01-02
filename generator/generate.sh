2a10d6800e8c416030036c0f3fa351f88767cbdc
Jim Pick <jim@jimpick.com>
2025-01-01 18:19:15 -0800

    Make templates fancier

--- generator/generate.sh
+++ generator/generate.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+# Uses: csvkit
+# brew install csvkit
+# https://csvkit.readthedocs.io/en/latest/
+
 #set -euxo pipefail
 set -euo pipefail
 
@@ -7,12 +11,23 @@ gen_new_file() {
   DIRNAME=$(dirname $1)
   BASENAME=$(basename $1)
   mkdir -p current/$DIRNAME
-  SERIAL=$(cat previous/$DIRNAME/$BASENAME | sed -n 's,; serial,,p')
+  SERIAL=$(cat previous/$DIRNAME/$BASENAME | sed -n -E 's,^[[:space:]]+([[:digit:]]+).*; serial,\1,p')
   if [ -z "$SERIAL" ]; then
     cp templates/$DIRNAME/$BASENAME current/$DIRNAME/$BASENAME
     return
   fi
-  cat ips.json | jq ".serial=$SERIAL" | minijinja-cli -f json templates/$DIRNAME/$BASENAME - > current/$DIRNAME/$BASENAME
+  cat << EOT > tmp/jq-cmd.txt
+  {
+    sites: \$sites[0],
+    ips: \$ips[0],
+    serial: "$SERIAL"
+  }
+EOT
+  jq -n \
+    --slurpfile sites tmp/sites-jim.json \
+    --slurpfile ips ips.json \
+    -f tmp/jq-cmd.txt > tmp/data.json
+  minijinja-cli templates/$DIRNAME/$BASENAME tmp/data.json > current/$DIRNAME/$BASENAME
   PREVSUM=$(cat previous/$DIRNAME/$BASENAME | sha512sum)
   CURSUM=$(cat current/$DIRNAME/$BASENAME | sha512sum)
   if [ "$PREVSUM" != "$CURSUM" ]; then
@@ -25,9 +40,24 @@ gen_new_file() {
       SERIAL="${DATE}01"
     fi
     #echo SERIAL: $SERIAL
-    cat ips.json | jq ".serial=$SERIAL" | minijinja-cli -f json templates/$DIRNAME/$BASENAME - > current/$DIRNAME/$BASENAME
+    cat << EOT > tmp/jq-cmd.txt
+    {
+      sites: \$sites[0],
+      ips: \$ips[0],
+      serial: "$SERIAL"
+    }
+EOT
+  jq -n \
+    --slurpfile sites tmp/sites-jim.json \
+    --slurpfile ips ips.json \
+    -f tmp/jq-cmd.txt > tmp/data.json
+    minijinja-cli templates/$DIRNAME/$BASENAME tmp/data.json  > current/$DIRNAME/$BASENAME
   fi
 }
+
+mkdir -p tmp
+
+csvjson sites/jim.csv > tmp/sites-jim.json
 
 for f in $(find templates -type f | sed 's,templates/,,'); do
   #echo $f

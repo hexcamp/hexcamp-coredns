70ec3aabcf00ad1683a73fadffc28d6536c0df50
Jim Pick <jim@jimpick.com>
2025-03-07 11:58:29 -0800

    Update corefiles/zones even when IP is missing

--- update-all.sh
+++ update-all.sh
@@ -1,22 +1,49 @@
 #! /bin/bash
 
-set -euxo pipefail
+set -euo pipefail
 
+set -x
 sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder
+set +x
 
-ssh minikube7 date
-ssh minikube8 date
-ssh minikube9 date
-ssh minikube10 date
+pushd generator
+./update-ips-from-tofu.sh
+popd
+
+IP_7=$(cat generator/ips.json | jq -r .minikube7_ip)
+IP_8=$(cat generator/ips.json | jq -r .minikube8_ip)
+IP_9=$(cat generator/ips.json | jq -r .minikube9_ip)
+IP_10=$(cat generator/ips.json | jq -r .minikube10_ip)
+
+set -x
+if [ -n "$IP_7" ]; then
+  ssh minikube7 date
+fi
+if [ -n "$IP_8" ]; then
+  ssh minikube8 date
+fi
+if [ -n "$IP_9" ]; then
+  ssh minikube9 date
+fi
+if [ -n "$IP_10" ]; then
+  ssh minikube10 date
+fi
 
 pushd generator
 ./copy-current-to-prev.sh
-./update-ips-from-tofu.sh
 ./generate.sh
 popd
 
 set +e
-./upload-minikube7.sh
-./upload-minikube8.sh
-./upload-minikube9.sh
-./upload-minikube10.sh
+if [ -n "$IP_7" ]; then
+  ./upload-minikube7.sh
+fi
+if [ -n "$IP_8" ]; then
+  ./upload-minikube8.sh
+fi
+if [ -n "$IP_9" ]; then
+  ./upload-minikube9.sh
+fi
+if [ -n "$IP_10" ]; then
+  ./upload-minikube10.sh
+fi

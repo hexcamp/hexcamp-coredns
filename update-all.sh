6d6eeba378de1c774d28fd7f5a1c09f0ffb6a85a
Jim Pick <jim@jimpick.com>
2025-03-19 09:57:25 -0700

    Update

--- update-all.sh
+++ update-all.sh
@@ -20,26 +20,33 @@ IP_8=$(cat generator/ips.json | jq -r .minikube8_ip)
 IP_9=$(cat generator/ips.json | jq -r .minikube9_ip)
 IP_10=$(cat generator/ips.json | jq -r .minikube10_ip)
 
+set +e
 if [ -n "$IP_7" ]; then
   echo "minikube7: http://$IP_7:9153/metrics"
   echo -n "           "
-  curl -s http://$IP_7:9153/metrics | grep ^coredns_build_info
+  timeout -v 5 curl -s http://$IP_7:9153/metrics | grep ^coredns_build_info
+  echo
 fi
 if [ -n "$IP_8" ]; then
   echo "minikube8: http://$IP_8:9153/metrics"
   echo -n "           "
-  curl -s http://$IP_8:9153/metrics | grep ^coredns_build_info
+  timeout -v 5 curl -s http://$IP_8:9153/metrics | grep ^coredns_build_info
+  echo
 fi
 if [ -n "$IP_9" ]; then
   echo "minikube9: http://$IP_9:9153/metrics"
   echo -n "           "
-  curl -s http://$IP_9:9153/metrics | grep ^coredns_build_info
+  timeout -v 5 curl -s http://$IP_9:9153/metrics | grep ^coredns_build_info
+  echo
 fi
 if [ -n "$IP_10" ]; then
   echo "minikube10: http://$IP_10:9153/metrics"
   echo -n "            "
-  curl -s http://$IP_10:9153/metrics | grep ^coredns_build_info
+  timeout -v 5 curl -s http://$IP_10:9153/metrics | grep ^coredns_build_info
+  echo
 fi
+set -e
+echo
 
 cd generator
 ./copy-current-to-prev.sh
@@ -49,19 +56,19 @@ cd ..
 set +e
 if [ -n "$IP_7" ]; then
   echo Uploading to minikube7
-  ./upload-minikube7.sh
+  timeout -v 15 ./upload-minikube7.sh
 fi
 if [ -n "$IP_8" ]; then
   echo Uploading to minikube8
-  ./upload-minikube8.sh
+  timeout -v 15 ./upload-minikube8.sh
 fi
 if [ -n "$IP_9" ]; then
   echo Uploading to minikube9
-  ./upload-minikube9.sh
+  timeout -v 15 ./upload-minikube9.sh
 fi
 if [ -n "$IP_10" ]; then
   echo Uploading to minikube10
-  ./upload-minikube10.sh
+  timeout -v 15 ./upload-minikube10.sh
 fi
 
 echo Done.

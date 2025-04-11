eb2240df9d62c76ed99b3430bce2d229157c3f27
Jim Pick <jim@jimpick.com>
2025-04-11 13:04:59 -0700

    Add rsync script

---
+++ rsync-from-ryzen9.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+DIR=/var/lib/rancher/k3s/storage/pvc-fe5e2630-8988-417f-a006-ccf61e2a902b_repair_repair-hexcamp-coredns/hexcamp-coredns/generator/
+
+rsync -vaP root@ryzen9:$DIR/current/ generator/current
+rsync -vaP root@ryzen9:$DIR/previous/ generator/previous

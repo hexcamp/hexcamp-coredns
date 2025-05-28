1e81414601f73c3809512e501d31b6b991584931
Jim Pick <jim@jimpick.com>
2025-05-27 17:00:47 -0700

    Debugging

--- update-zones.sh
+++ update-zones.sh
@@ -72,23 +72,21 @@ set +e
 mkdir -p /root/.config/rclone
 touch /root/.config/rclone/rclone.conf
 
-trap "" INT
-
 if [ -n "$IP_7" ]; then
   echo Uploading to minikube7
-  #/usr/bin/timeout -v -s INT 30 ./upload-minikube7-zone-generator.sh 2>&1
+  /usr/bin/timeout -v 30 ./upload-minikube7-zone-generator.sh 2>&1
 fi
 if [ -n "$IP_8" ]; then
   echo Uploading to minikube8
-  #/usr/bin/timeout -v -s INT 30 ./upload-minikube8-zone-generator.sh 2>&1
+  /usr/bin/timeout -v 30 ./upload-minikube8-zone-generator.sh 2>&1
 fi
 if [ -n "$IP_9" ]; then
   echo Uploading to minikube9
-  #/usr/bin/timeout -v -s INT 30 ./upload-minikube9-zone-generator.sh 2>&1
+  /usr/bin/timeout -v 30 ./upload-minikube9-zone-generator.sh 2>&1
 fi
 if [ -n "$IP_10" ]; then
   echo Uploading to minikube10
-  #/usr/bin/timeout -v -s INT 30 ./upload-minikube10-zone-generator.sh 2>&1
+  /usr/bin/timeout -v 30 ./upload-minikube10-zone-generator.sh 2>&1
 fi
 
 echo "Done."
